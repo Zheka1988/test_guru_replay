378acd7fc7d4af62edc8cf83c89aa20215f9f5a5
Zheka1988 <kveladze.kz@mail.ru>
2020-04-16 03:30:13 -0700

    added version, time_zone, locale and static pages about project and author

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,15 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative 'initializers/version'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TestGuruReplay
+  include Version
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -15,5 +18,13 @@ module TestGuruReplay
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = "Astana"
+
+    config.i18n.default_locale = :ru
+    config.i18n.fallbacks = {'ru' => 'en'}
+
+    # config.before_initialize { TestGuruReplay::VERSION }
   end
+
 end
