880ef7ad34fb421d82bc998d307f999e5727ba8f
Zheka1988 <kveladze.kz@mail.ru>
2020-04-18 13:28:09 -0700

    добавил модели, ограничения на null, значения по умолчанию для Answer.correct и Test.level

---
+++ db/migrate/20200418202146_add_tables_value_default.rb
@@ -0,0 +1,6 @@
+class AddTablesValueDefault < ActiveRecord::Migration[6.0]
+  def change
+    change_column_default(:tests, :level, from: 0, to: 1)
+    change_column_default(:answers, :correct, from: true, to: false)
+  end
+end
