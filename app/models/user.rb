98d8bbb8fe903dce0b7415bd71bf33e26b81cb11
Zheka1988 <kveladze.kz@mail.ru>
2020-04-24 09:54:29 -0700

    добавил автора теста, добавил валидации и скоупы

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  # has_many :traveled_tests
   has_many :tests
 
+  has_many :own_tests, class_name: "Test", dependent: :nullify
   has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
-  def tests_by_level(level)
-    # Test.joins(:traveled_tests).where(level: level, 
-    #   traveled_tests: { user_id: self.id })   
-    tests.where(level: level)
-  end
 
+  scope :tests_by_level, ->(level) { tests.where(level: level)}
+
+  validates :name, presence: true
+  validates :password, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,41 @@ User.create!(name: 'Vova', password: '123')
 User.create!(name: 'Kolya', password: '123')
 User.create!(name: 'Vika', password: '123')
 
-users_object = User.all
+user = User.first
 
+Category.create!(title: "Backend")
+Category.create!(title: "Frontend")
+Category.create!(title: "Mobile development")
 
-3.times do |i|
-  Category.create! title: "Category_#{i+1}"
+Test.create!(title: 'Ruby', level: 6, 
+            category_id: Category.find_by(title: 'Backend').id, author_id: user.id)
+Test.create!(title: 'C++', level: 5, 
+            category_id: Category.find_by(title: 'Backend').id, author_id: user.id)
+Test.create!(title: 'HTML', #level: 1, 
+            category_id: Category.find_by(title: 'Frontend').id, author_id: user.id)
+Test.create!(title: 'CSS', level: 2, 
+            category_id: Category.find_by(title: 'Frontend').id, author_id: user.id)
+Test.create!(title: 'Java', level: 4, 
+            category_id: Category.find_by(title: 'Mobile development').id, author_id: user.id)
+
+Question.create!(body: 'HTML this?',  test_id: Test.find_by(title: 'HTML').id)
+Question.create!(body: 'C++ this?',  test_id: Test.find_by(title: 'C++').id)
+Question.create!(body: 'Ruby this?',  test_id: Test.find_by(title: 'Ruby').id)
+Question.create!(body: 'Rails this?',  test_id: Test.find_by(title: 'Ruby').id)
+
+questions = Question.all
+
+4.times do |i|
+  Answer.create(body: 'Verno', question_id: questions[i].id, correct: true)
+  Answer.create(body: 'Neverno', question_id: questions[i].id)  
 end
 
-5.times do |i|
-  Test.create! title: "Test_#{i+1}", level: i, 
-    category_id: Category.find(rand(1..3)).id
-end
-
-test_objects = Test.all
-
-question_objects = Array.new(5) do |i|
-  Question.create!(body: "Question ##{i+1}", test_id: test_objects[i].id )
-end
-
-5.times do |i|
-  Answer.create!(question_id: question_objects[i].id)
-end
-
-# 3.times do |i|
-#   TraveledTest.create!(user_id: users_object[i].id, test_id: test_objects[i].id )
+# def randomizer
+#   Question.offset(rand(Question.count)).first.id
 # end
-#   TraveledTest.create!(user_id: users_object[0].id, test_id: test_objects[1].id )
+
+# 10.times do |i| 
+#   question_id = randomizer
+#   Answer.create(body: 'Verno', question_id: question_id, correct: true)
+#   Answer.create(body: 'Neverno', question_id: question_id)
+# end
