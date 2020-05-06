a9ecc6838ee738e46fb0a389d9eab5c3380f93ed
Zheka1988 <kveladze.kz@mail.ru>
2020-05-05 23:40:23 -0700

    Devise, разграничение прав доступа

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tests#index'
 
-  resources :tests do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, :controllers => { sessions: 'sessions' }
+
+  resources :tests, only: :index do
     post :start, on: :member
   end
 
@@ -13,12 +12,12 @@ Rails.application.routes.draw do
     get :result, on: :member
   end 
 
-  
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  get :exit, to: 'sessions#exit'
-
-  resources :users, only: [:create, :new]
-  resources :sessions, only: %i[create exit]
+  namespace :admin do
+    resources :tests do
+      resources :questions, shallow: true do
+        resources :answers, shallow: true
+      end
+    end
+  end
 
 end
