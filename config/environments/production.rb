f936084099861146c25a6cd236845000085bd50a
Zheka1988 <kveladze.kz@mail.ru>
2020-05-17 19:46:25 -0700

    изменения mailer in config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'floating-beyond-86012.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'secret-hollows-99781.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
